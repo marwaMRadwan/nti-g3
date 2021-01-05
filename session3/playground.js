40ee2a52c3be8a84c353c34e63956295216732bb
marwa Radwan <marwaradwan872@hotmail.com>
2021-01-05 15:34:25 +0200

    s3

--- session3/playground.js
+++ session3/playground.js
@@ -1,8 +1,58 @@
-let name = "marwa"
-let age = 36
-str = `my name is ${name} and my age is ${age}`
-console.log(str);
+// let name = "marwa"
+// let age = 36
+// str = `my name is ${name} and my age is ${age}`
+// console.log(str);
 
-x=[1, 2, 3, 4, 5, 6, 7]
+// x=[1, 2, 3, 4, 5, 6, 7]
 
-x.splice(2,2)
+// x.splice(2,2)
+
+// arr = [1, 2, 3, 4, 5, 4, 5, 5,8, 90]
+// x = arr.filter(ele => {return ele == 5 } )
+// console.log(x);
+// x = arr.findIndex(ele=>{
+//     return ele == 5
+// })
+// console.log(x);
+
+// arr.forEach((ele, index)=>{
+//     console.log(ele, " ", index);
+// })
+//find filter  indexof
+// x = arr.find(ele=>{
+//     return ele == 15
+// })
+// console.log(x)
+
+
+// names = [
+//     'ahmed', 'mohamed', 'hossam', 'ibrahem', 
+//     'george', 'mostafa', 'saher', 'shehab' 
+// ]
+//[0,1,2,3,5]
+// indexes = []
+// names.forEach((element, i) => {
+//     if(element.includes('m')) indexes.push({ name: element, index: i })  
+// });
+// x = names.findIndex(name => { 
+//     return name.includes('m')
+// })
+
+//console.log(indexes);
+// indexes = names.filter( (name, i) => { //i=0
+// return name.includes('m')
+// })
+// indexes.forEach(element => {
+//     names.findIndex(name => { return name=element })
+// });
+//console.log(indexes);
+
+// x = names.filter( name => { return name.includes('m') })
+// y = names.findIndex(name => { return name.includes('m') })
+// console.log(`
+// x= ${x}
+// y= ${y}
+// z= ${z}
+// `);
+// names => [x,y,z,b,c]
+// filter => array [x,y,z]
